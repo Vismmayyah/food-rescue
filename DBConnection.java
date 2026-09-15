ae8f54c49aab9c912c7eaffb6007c8b1c5a24bda
NeerajaK04 <lakshmineeraj720@gmail.com>
2026-09-15 21:27:52 +0530

    Add files via upload

---
+++ DBConnection.java
@@ -0,0 +1,31 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+
+    private static final String URL = "jdbc:mysql://localhost:3306/FoodRescue";
+    private static final String USER = "root";
+    private static final String PASSWORD = "";
+
+    public static Connection getConnection() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            Connection connection =
+                    DriverManager.getConnection(URL, USER, PASSWORD);
+
+            System.out.println("Database connected successfully!");
+            return connection;
+
+        } catch (ClassNotFoundException e) {
+            System.out.println("MySQL driver not found!");
+            e.printStackTrace();
+        } catch (SQLException e) {
+            System.out.println("Database connection failed!");
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+}
